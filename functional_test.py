2cf6538b494943e76f456bb47ca1b62f42f4ff35
Yasir Ali <yasir.ali@asynchrony.com>
2019-08-28 17:23:15 -0500

    Adding 'setup' and 'teardown':
    setUp and tearDown methods are special methods provided by unititest.
    - setUp method runs before every test
    - tearDown method runs after every test
    
    moving browser.quit() in tearDown will ensure that the browser is closed regardless of success or failure of the test.

--- functional_test.py
+++ functional_test.py
@@ -2,13 +2,16 @@ from selenium import webdriver
 import unittest
 
 class HomePageTest(unittest.TestCase): # unittest.TestCase means we are inhering from TestCase class
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-  # test methods are recognized by any method that starts with 'test'
-  def test_home_page(self):
-    browser = webdriver.Chrome()
-    browser.get('http://localhost:8000')
-    self.assertIn('To-Do', browser.title)
-    browser.quit()
+    def tearDown(self):
+        self.browser.quit()
+        
+    # test methods are recognized by any method that starts with 'test'
+    def test_home_page(self):
+        browser.get('http://localhost:8000')
+        self.assertIn('To-Do', browser.title)
 
 if __name__ == '__main__':
   unittest.main()

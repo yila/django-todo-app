3ee701f4e6fd5c464ee50452f0cbff107185b12a
Yasir Ali <yasir.ali@asynchrony.com>
2019-08-28 17:13:04 -0500

    Using 'unittest' library

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,14 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class HomePageTest(unittest.TestCase): # unittest.TestCase means we are inhering from TestCase class
+
+  # test methods are recognized by any method that starts with 'test'
+  def test_home_page(self):
+    browser = webdriver.Chrome()
+    browser.get('http://localhost:8000')
+    self.assertIn('To-Do', browser.title)
+    browser.quit()
+
+if __name__ == '__main__':
+  unittest.main()

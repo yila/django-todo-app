2e1c066664e73fd817b10e54a2533696b299d607
Yasir Ali <yasir.ali@asynchrony.com>
2019-08-29 14:04:31 -0500

    Failing test for view to return html - though not failing on assertion:
    Failure is on how the view function has been defined.  The next step should be to fix that error message.

--- lists/tests.py
+++ lists/tests.py
@@ -9,3 +9,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page(self):
         resolveResult = resolve('/')
         self.assertEqual(resolveResult.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do List', html)
+        self.assertTrue(html.endswith('</html>'))

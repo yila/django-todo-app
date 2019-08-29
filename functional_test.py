fb5f481b8208e2ab7b69d626a5ff4813e56e2cff
Yasir Ali <yasir.ali@asynchrony.com>
2019-08-29 11:12:27 -0500

    Fixed Mistake: Using keyword 'self' when accessing the browser

--- functional_test.py
+++ functional_test.py
@@ -7,11 +7,12 @@ class HomePageTest(unittest.TestCase): # unittest.TestCase means we are inhering
 
     def tearDown(self):
         self.browser.quit()
-        
+
     # test methods are recognized by any method that starts with 'test'
     def test_home_page(self):
-        browser.get('http://localhost:8000')
-        self.assertIn('To-Do', browser.title)
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish this test! - forcing to failing on purpose')
 
 if __name__ == '__main__':
   unittest.main()

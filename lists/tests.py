dcd1611bb073a5da035b48a01f477efd3601dc4e
Yasir Ali <yasir.ali@asynchrony.com>
2019-08-29 12:15:59 -0500

    Adding a failing test to show home page does not exist (404)

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
 
 # Create your tests here.
 # To run the test execute the following from 'django-todo-app' folder:
 # python manage.py test
 class HomePageTest(TestCase):
-    def test_smoke_test(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolves_to_home_page(self):
+        resolveResult = resolve('/')
+        self.assertEqual(resolveResult.func, home_page)

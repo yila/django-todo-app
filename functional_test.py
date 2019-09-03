41314c0300e03619a4be26a0425efbddc408a756
Yasir Ali <yasir.ali@asynchrony.com>
2019-09-03 11:15:04 -0500

    Added basic structure to the to-do page via functional tests

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class HomePageTest(unittest.TestCase): # unittest.TestCase means we are inhering from TestCase class
@@ -10,8 +12,34 @@ class HomePageTest(unittest.TestCase): # unittest.TestCase means we are inhering
 
     # test methods are recognized by any method that starts with 'test'
     def test_home_page(self):
+        # Nikkivisits a great to-do app site:
         self.browser.get('http://localhost:8000')
+
+        # She sees the title and it matches with what she expected
         self.assertIn('To-Do', self.browser.title)
+
+        # There is also a heading on the page where she can add here to-do items
+        heading_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('My To Do List', heading_text)
+
+        # she finds a spot to add a to-do items
+        input_box = self.browser.find_element_by_id('id_new_item')
+
+        # she sees grayed out text guiding her to enter text in that spot
+        self.assertEqual(input_box.get_attribute('placeholder'),
+            'Enter a to-do item')
+
+        # she types in 'Stain deck stairs'
+        input_box.send_keys('Stain deck stairs')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_to_do_list')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Stain deck stairs' for row in rows)
+        )
+
         self.fail('Finish this test! - forcing to failing on purpose')
 
 if __name__ == '__main__':

bdb52607b2e5295e43b9f7da48a913b9484bd251
Yasir Ali <yasir.ali@asynchrony.com>
2019-09-03 15:52:17 -0500

    Updated html to be able to submit a form + updated view file to populate template.

--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        return HttpResponse('foo')
+        return render(request, 'index.html', {'new_item_text': request.POST['item_text']})
     return render(request, 'index.html')

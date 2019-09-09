73bcae94809cca83b8b973d114f0f6b27b5b3022
Yasir Ali <yasir.ali@asynchrony.com>
2019-09-09 16:48:17 -0500

    Failing functional test - Adding a unit test to make sure item is not saved on get

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from lists.models import ToDoItem
 
 # Create your tests here.
 # To run the test execute the following from 'django-todo-app' folder:
@@ -14,6 +15,11 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
 
-    def test_handles_post_request(self):
+    def test_can_save_a_post_request(self):
         post_response = self.client.post('/', {'item_text': 'my first to-do item'})
+        self.assertEqual(ToDoItem.objects.count(), 1)
         self.assertIn('my first to-do item', post_response.content.decode())
+
+    def test_do_not_save_items_on_a_get_request(self):
+        self.client.get('/')
+        self.assertEqual(ToDoItem.objects.count(), 0)

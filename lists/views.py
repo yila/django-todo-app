70ed0788c9f644ccbc96eb3032c2ba2ffd9dee76
Yasir Ali <yasir.ali@asynchrony.com>
2019-09-09 16:47:50 -0500

    Failing functional test - Adding saving to DB to view

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from lists.models import ToDoItem
 
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        return render(request, 'index.html', {'new_item_text': request.POST['item_text']})
+        toDoItem = ToDoItem()
+        toDoItem.description = request.POST.get('item_text')
+        toDoItem.save()
+        
+        return render(request, 'index.html', {'new_item_text':toDoItem.description})
     return render(request, 'index.html')

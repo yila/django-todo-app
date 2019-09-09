81918c9e36110421b33588285e854bff1a5da5f8
Yasir Ali <yasir.ali@asynchrony.com>
2019-09-09 12:21:00 -0500

    Failing functional test - DB migration
    - run command "python manage.py makemigrations" to add fields to ToDoItem model

--- lists/models.py
+++ lists/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ToDoItem(models.Model):
+    # After adding the inheritance, run the following command:
+    # python manage.py makemigrations
+    description = models.TextField()
     pass
